e89355c6de57e55e9b08877e9a00fd5c440a177f
simon <tran.simon2@gmail.com>
2023-01-25 20:08:36 -0500

    Added game activity layout

--- android/src/com/inf8405/tp1/AndroidLauncher.kt
+++ android/src/com/inf8405/tp1/AndroidLauncher.kt
@@ -1,13 +1,18 @@
 package com.inf8405.tp1
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
         val config = AndroidApplicationConfiguration()
-        initialize(UnblockMe(), config)
+
+        val layout = findViewById<ConstraintLayout>(R.id.root_layout)
+        layout.addView(initializeForView(UnblockMe(), config))
     }
 }

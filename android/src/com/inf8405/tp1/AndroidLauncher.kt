c522524a612a4dbf800e019cd28baa54df3f57bb
simon <tran.simon2@gmail.com>
2023-01-25 19:16:40 -0500

    init libgdx

---
+++ android/src/com/inf8405/tp1/AndroidLauncher.kt
@@ -0,0 +1,13 @@
+package com.inf8405.tp1
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(UnblockMe(), config)
+    }
+}

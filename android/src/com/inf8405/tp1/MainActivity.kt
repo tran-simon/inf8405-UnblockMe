9b85d0814bbe4493f66834244c8cf69559e110bb
pritapat <pritam.patel@polymtl.ca>
2023-02-07 16:51:14 -0500

    completed main menu with theming

--- android/src/com/inf8405/tp1/MainActivity.kt
+++ android/src/com/inf8405/tp1/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.inf8405.tp1
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
 import android.view.View
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,4 +19,36 @@ class MainActivity : AppCompatActivity() {
     fun startGame(view: View) {
         startActivity(Intent(this,  AndroidLauncher::class.java))
     }
+
+    fun showAbout(view: View) {
+        val aboutFragment = AboutDialogFragment()
+        aboutFragment.show(supportFragmentManager, "about")
+    }
+
+    fun exitGame(view: View) {
+        finish();
+        exitProcess(0);
+    }
+}
+
+class AboutDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            // Get the layout inflater
+            val inflater = requireActivity().layoutInflater;
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_about, null))
+                // Add action buttons
+                .setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+
+                    })
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
 }
